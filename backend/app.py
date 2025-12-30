3189a496aafbac29ca3ecfe3c7ffc035f051a312
Sudharsan <sudharsanmv7@gmail.com>
2025-12-30 16:13:50 +0530

    Updated Cors

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,10 @@ import io
 import os
 
 app = Flask(__name__)
-CORS(app)
+CORS(app,origins=[
+    "https://prepix.netlify.app",
+    "http://localhost:5173"
+])
 
 
 def process_image_file(file_stream, output_ext):
